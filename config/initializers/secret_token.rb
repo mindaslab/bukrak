aaf54b031d041edf2820f774d00d945a3cdbaabc
Karthikeyan A K <mindaslab@mail.com>
2013-02-15 12:07:59 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bukrak::Application.config.secret_token = 'e5bea31c8376d842207858e9c84839656a9a3013b8cdd62f39f99dd8af59c9529ff7d233dc018a02dfd9b07811d05ec8cb6a2317f9e1d6f8074938d27bab3274'
---
+++ test/unit/helpers/bukrak_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BukrakHelperTest < ActionView::TestCase
+end
